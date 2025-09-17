72a62052982449b95920b1e13c7b6b46c6e4a3a2
Sebas-p-s <sebastianpachon970@gmail.com>
2025-09-17 14:02:00 -0500

    Creo la funcion calcular promedios

--- Desempeño-corporativo/Calificacion-corporativo.py
+++ Desempeño-corporativo/Calificacion-corporativo.py
@@ -2,6 +2,8 @@
 ##creare una tupla para que la lista de despeños este en uan sola linea
 lista_de_los_desempenos = ("Calidad","Puntualidad","Colaboracion","Eficiencia")
 empleados_desempenos = []
+promedios = {}
+lista_promedios = []
 def registar_empleados():
     opt_registrar_empleados = True
     while opt_registrar_empleados:
@@ -16,10 +18,9 @@ def registar_empleados():
                 while respuesta <= 0 or respuesta > 10:
                     respuesta = (float(input(f"Ingrese el puntaje de {nombre_empleado} basado en su {i}: ")))
                     if respuesta <= 0 or respuesta > 10:
-                        print("Dato no valido")
-                        
+                        print("Dato no valido")  
+                ##Esto lo que hara sera unir la respuesta a el diccionario empleado ejemplo: empleado['calidad']= 8.5
                 empleado[i] = respuesta
-
             empleados_desempenos.append(empleado)
         salir = ""
         while salir not in ("Si", "No"):
@@ -31,7 +32,30 @@ def registar_empleados():
                 pass
             else:
                 print("Respuesta no valida.")
+
+def calcular_promedios():
+    if empleados_desempenos:
+        for empleado in empleados_desempenos:
+            suma_promedios = 0
+            promedio = 0
+            for categoria in lista_de_los_desempenos:
+                suma_promedios += empleado[categoria]
+            promedio = suma_promedios / len(lista_de_los_desempenos)
+            promedio = round(promedio, 1)
+            promedios[empleado['Nombre']] = promedio
+            lista_promedios.append(promedio)
+        indi = 0
+        for i in promedios:
+            print(f"{i} : {lista_promedios[indi]}")
+            indi += 1
+                       
+    else: 
+        print("No hay ningun dato")
+
 registar_empleados()
+calcular_promedios()
+        
+        
                     
                 
 
